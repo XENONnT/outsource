0e54699a16fb1ab5e463db4a2e944b9096a3015a
Lanqing Yuan <yuanlq@uchicago.edu>
2023-09-13 12:02:59 +0200

    `raw_records` RSE surgery (#71)
    * put back existing rses
    
    * debug
    
    * adding rse sites
    
    * removed hardcoded UC_OSG_USERDISK
    
    * more mem and disk

--- outsource/Outsource.py
+++ outsource/Outsource.py
@@ -47,15 +47,16 @@ class Outsource:
         'UC_OSG_USERDISK':    {'expr': 'GLIDEIN_Country == "US"'},
         'UC_DALI_USERDISK':   {'expr': 'GLIDEIN_Country == "US"'},
         'UC_MIDWAY_USERDISK': {'expr': 'GLIDEIN_Country == "US"'},
-        #'CCIN2P3_USERDISK':   {'desired_sites': 'CCIN2P3',  'expr': 'GLIDEIN_Site == "CCIN2P3"'},
-        #'CNAF_TAPE_USERDISK': {},
-        #'CNAF_USERDISK':      {'desired_sites': 'CNAF',     'expr': 'GLIDEIN_Site == "CNAF"'},
-        #'LNGS_USERDISK':      {},
-        #'NIKHEF2_USERDISK':   {'desired_sites': 'NIKHEF',   'expr': 'GLIDEIN_Site == "NIKHEF"'},
-        #'NIKHEF_USERDISK':    {'desired_sites': 'NIKHEF',   'expr': 'GLIDEIN_Site == "NIKHEF"'},
-        #'SURFSARA_USERDISK':  {'desired_sites': 'SURFsara', 'expr': 'GLIDEIN_Site == "SURFsara"'},
-        #'WEIZMANN_USERDISK':  {'desired_sites': 'Weizmann', 'expr': 'GLIDEIN_Site == "Weizmann"'},
-        #'SDSC_USERDISK': {'expr': 'GLIDEIN_ResourceName == "SDSC-Expanse"'}
+        'CCIN2P3_USERDISK':   {'desired_sites': 'CCIN2P3',  'expr': 'GLIDEIN_Site == "CCIN2P3"'},
+        'CNAF_TAPE_USERDISK': {},
+        'CNAF_USERDISK':      {'desired_sites': 'CNAF',     'expr': 'GLIDEIN_Site == "CNAF"'},
+        'LNGS_USERDISK':      {},
+        'NIKHEF2_USERDISK':   {'desired_sites': 'NIKHEF',   'expr': 'GLIDEIN_Site == "NIKHEF"'},
+        'NIKHEF_USERDISK':    {'desired_sites': 'NIKHEF',   'expr': 'GLIDEIN_Site == "NIKHEF"'},
+        'SURFSARA_USERDISK':  {'desired_sites': 'SURFsara', 'expr': 'GLIDEIN_Site == "SURFsara"'},
+        'WEIZMANN_USERDISK':  {'desired_sites': 'Weizmann', 'expr': 'GLIDEIN_Site == "Weizmann"'},
+        'SDSC_USERDISK': {'expr': 'GLIDEIN_ResourceName == "SDSC-Expanse"'},
+        'SDSC_NSDF_USERDISK': {'expr': 'GLIDEIN_Country == "US"'},
     }
 
     # transformation map (high level name -> script)
@@ -79,12 +80,12 @@ class Outsource:
                   'download': dict(name='download', disk=2_000_000),
                   'records': dict(name='records', memory=5_000),
                   'peaklets': dict(name='peaklets', memory=20_000, disk=40_000_000),
-                  'peak_basics': dict(name='peak_basics', memory=20_000, disk=40_000_000),
-                  'event_info_double': dict(name='events', memory=24_000, disk=100_000_000, cores=1),
-                  'peak_basics_he': dict(name='peaksHE', memory=8_000, cores=1),
-                  'hitlets_nv': dict(name='nv_hitlets', memory=5_000),
-                  'events_nv': dict(name='nv_events', memory=8_000, disk=2_000_000),
-                  'events_mv': dict(name='mv', memory=1_700),
+                  'peak_basics': dict(name='peak_basics', memory=40_000, disk=40_000_000),
+                  'event_info_double': dict(name='events', memory=60_000, disk=100_000_000),
+                  'peak_basics_he': dict(name='peaksHE', memory=8_000),
+                  'hitlets_nv': dict(name='nv_hitlets', memory=40_000, disk=40_000_000),
+                  'events_nv': dict(name='nv_events', memory=60_000, disk=100_000_000),
+                  'events_mv': dict(name='mv', memory=40_000, disk=40_000_000),
                   'afterpulses': dict(name='ap', memory=3_000),
                   'led_calibration': dict(name='led', memory=4_000)
                   }
@@ -319,7 +320,7 @@ class Outsource:
                 # determine the job requirements based on the data locations
                 # for standalone downloads, only target us
                 if dbcfg.standalone_download:
-                    rses = ['UC_OSG_USERDISK']
+                    rses = config.get('Outsource', 'raw_records_rse').split(',')
                 sites_expression, desired_sites = self._determine_target_sites(rses)
 
                 # hs06_test_run limits the run to a set of compute nodes at UChicago with a known HS06 factor
