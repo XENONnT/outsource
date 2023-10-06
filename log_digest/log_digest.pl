9c1c0dfbc5a9679747b1745e77a3bde1565ebe08
Cecilia Ferrari <ferraric@midway3-login3.rcc.local>
2023-10-06 03:20:59 -0500

    implementing options for common error patterns and printing stats

--- log_digest/log_digest.pl
+++ log_digest/log_digest.pl
@@ -4,15 +4,27 @@
 use strict;
 use warnings;
 use Time::HiRes qw(gettimeofday tv_interval);
+use Getopt::Long;
 
 # Error patterns to be searched
 my @known_error_patterns = (
-    #qr/(\bERROR\b: .+)/,
-    #qr/(\bERROR - Failed to download file\b)/,
-    qr/(.+stale file handle .+)/,
     qr/(\bERROR\b.+)/,
 );
-
+# help message
+sub display_help {
+    print "Usage: $0 [options] <filename>\n";
+    print "Options:\n";
+    print "  --error_list <filename>     Searches for error patterns stored in <filename> file.\n";
+    print "  --help     Show this help message\n";
+    exit(0);
+}
+GetOptions(
+    'error_list|e=s' => sub {
+        my ($opt_value) = @_;
+        push @known_error_patterns, $opt_value;
+    },
+    'help|h' => sub {display_help(); },
+) or die "Usage: $0 <options> <filename>\n";
 
 my $start_time = [gettimeofday];
 my $args_string = join(" ", @ARGV);
@@ -21,6 +33,7 @@ print "\nDigesting: $args_string\n\n";
 # initalization of hashes
 my %errors = ();
 my %headers = ();
+my %known_error_stats = ();
 # nuisance variables
 my $first_line = 1;
 my $current_file = "0";
@@ -45,6 +58,8 @@ while (<>) {
             unless ($errors{$current_file}{$error_msg}) {
                 $errors{$current_file}{$error_msg} = $.; # Mark error as processed
             }
+            $known_error_stats{$current_file}{$pattern} //= 0;
+            $known_error_stats{$current_file}{$pattern} += 1;
         }
     }
 }
@@ -67,17 +82,28 @@ foreach my $file (keys %errors) {
 
 # Print parsing output
 foreach my $file (keys %similar_messages) {
-    print "========= Output for: $file =========\n";
+    print "======================= Output for: $file =======================\n";
+    # Print stats
+    print("Error pattern stats:\n");
+    print("-----------------------------------------------------------------\n");
+    foreach my $error_pattern (keys %{$known_error_stats{$file}}){
+        my $stats = $known_error_stats{$file}{$error_pattern};
+        if ($error_pattern ne "qr/(\bERROR\b.+)/") {$error_pattern = "ERROR"}
+        print("- \e[1m$error_pattern\e[0m: found $stats times in the logfile\n");
+    }
+    print("-----------------------------------------------------------------\n");
     foreach my $key_err (keys %{$similar_messages{$file}}) {
         print "Error msg: $key_err \t in lines [";
         foreach my $line (sort @{$similar_messages{$file}{$key_err}}) {
             print "$line, ";
-        };
+        }
     print "]\n";
-    };
+    }
 print "\n";
-};
+}
 
 my $end_time = [gettimeofday];
 my $execution_time = tv_interval($start_time, $end_time);
 print "Execution time: $execution_time s\n";
+
+
