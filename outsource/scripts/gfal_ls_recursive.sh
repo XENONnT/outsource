8f13f8e7e3c1d4824d453432a593399c08bd21c3
Dacheng Xu <xudctron@gmail.com>
2024-11-16 19:55:28 -0500

    List a directory in DAV (#235)

---
+++ outsource/scripts/gfal_ls_recursive.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+recursive_ls() {
+    local path=$1
+    gfal-ls $path | while read -r item; do
+        if [ $path = $item ]; then
+            echo $item
+        else
+            full=$path/$item
+            if gfal-ls $full &>/dev/null; then
+                recursive_ls $full
+            else
+                exit 1
+            fi
+        fi
+    done
+}
+
+remote_path=davs://xenon-gridftp.grid.uchicago.edu:2880/xenon/scratch/$USER/$1
+recursive_ls $remote_path 2>/dev/null
