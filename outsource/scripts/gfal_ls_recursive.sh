a8f0012ac0c70f5800b4eeb246df815dd829cb70
dachengx <dx2227@columbia.edu>
2025-02-14 08:24:19 -0600

    Simplify `gfal_ls_recursive.sh`

--- outsource/scripts/gfal_ls_recursive.sh
+++ outsource/scripts/gfal_ls_recursive.sh
@@ -22,16 +22,4 @@ recursive_ls() {
 }
 
 remote_path=davs://xenon-gridftp.grid.uchicago.edu:2880/xenon/scratch/$USER/$1
-workflow_path=/ospool/uc-shared/project/xenon/$USER/workflows/$1/runs
-
-# list the combine jobs for lower level
-combine=$(tree -if $workflow_path | grep combine | awk -F'/' '{print $(NF-2) "/" $(NF-1)}' | sort | uniq)
-for path in $combine; do
-    recursive_ls $remote_path/runs/$path 2>/dev/null
-done
-
-# list the upper jobs for upper level
-upper=$(tree -if $workflow_path | grep upper | awk -F'/' '{print $(NF-2) "/" $(NF-1)}' | sort | uniq)
-for path in $upper; do
-    recursive_ls $remote_path/runs/$path 2>/dev/null
-done
+recursive_ls $remote_path 2>/dev/null
