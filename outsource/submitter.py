e9cb60a848dc4354b1e9b323954be6f0fd782744
Dacheng Xu <xudctron@gmail.com>
2025-02-20 19:33:16 -0500

    Increase 20 percent disk/memory in next job retry (#266)
    * Increase 20 percent disk/memory in next job retry
    
    * Add option about increment percent

--- outsource/submitter.py
+++ outsource/submitter.py
@@ -248,23 +248,22 @@ class Submitter:
         # The first dagman_static_retry memory/disk will be the same to the first attempt
         # The memory/disk will be increased by the number of retries afterwards
         dagman_static_retry = uconfig.getint("Outsource", "dagman_static_retry", fallback=0)
+        resources_increment = uconfig.getfloat("Outsource", "resources_increment", fallback=0.2)
         if dagman_static_retry == 0:
-            extra_retry = "(DAGNodeRetry + 1)"
-        elif dagman_static_retry == 1:
             extra_retry = "DAGNodeRetry"
         else:
-            extra_retry = f"(DAGNodeRetry - {dagman_static_retry - 1})"
+            extra_retry = f"(DAGNodeRetry - {dagman_static_retry})"
         memory_str = (
             "ifthenelse(isundefined(DAGNodeRetry) || "
             f"DAGNodeRetry <= {dagman_static_retry}, "
             f"{int(memory)}, "
-            f"{extra_retry} * {int(memory)})"
+            f"({extra_retry} * {resources_increment} + 1) * {int(memory)})"
         )
         disk_str = (
             "ifthenelse(isundefined(DAGNodeRetry) || "
             f"DAGNodeRetry <= {dagman_static_retry}, "
             f"{int(disk * 1_000)}, "
-            f"{extra_retry} * {int(disk * 1_000)})"
+            f"({extra_retry} * {resources_increment} + 1) * {int(disk * 1_000)})"
         )
         job.add_profiles(Namespace.CONDOR, "request_disk", disk_str)
         job.add_profiles(Namespace.CONDOR, "request_memory", memory_str)
